015b9bb969db5f640d60793c9ec754b2515695f8
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-09-18 20:51:13 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+let amPm = document.getElementById("am-pm");
+
+setInterval(() => {
+    let secondeValue= parseInt(sec.textContent);
+    let minuteValue= parseInt(min.textContent);
+    secondeValue++;
+
+    if (secondeValue === 60) {
+        secondeValue = 0;
+        minuteValue++;
+    }
+    
+    if (minuteValue > 12 ) {
+        minuteValue = 1;
+        if ( amPm.textContent === "am") 
+            amPm.textContent = "pm";
+        else if ( amPm.textContent === "pm")
+            amPm.textContent = "am";
+    }
+    
+    sec.textContent = (secondeValue < 10 ) ? '0' + secondeValue : secondeValue;
+    min.textContent = (minuteValue < 10) ? '0' + minuteValue : minuteValue;
+}, 1000);
